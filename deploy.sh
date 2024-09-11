c9ceb280935ee656ac48faa481a8fa3c57039062
anish-singh-rawat <www.computernetwork@gmail.com>
2024-09-11 12:14:35 +0530

    first commit

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+cargo build --release --target wasm32-unknown-unknown --package crud_backend
+
+candid-extractor target/wasm32-unknown-unknown/release/crud_backend.wasm > src/crud_backend/crud_backend.did
+
+dfx deploy crud_backend
